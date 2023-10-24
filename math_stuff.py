e91c8ffea623943a5435d7bc44ee9b9029949556
LuisFPR04 <luisfpera@gmail.com>
2023-10-23 22:24:45 -0300

    commit

--- math_stuff.py
+++ math_stuff.py
@@ -1,1 +1,7 @@
 import numpy as np
+
+def rotation(alpha):
+    row_1 = [np.cos(alpha), -np.sin(alpha)]
+    row_2 = [np.sin(alpha), np.cos(alpha)]
+
+    return [row_1, row_2]
--- transfor_1.py
+++ transfor_1.py
@@ -1,22 +1,28 @@
 from math_stuff import *
 from manim import *
+from numpy import sqrt
 
 class vectorPrueba(LinearTransformationScene):
 
     def __init__(self):
         LinearTransformationScene.__init__(
             self,
-            leave_ghost_vectors=True,
+            show_coordinates=True,
+            show_basis_vectors=False,
+            include_background_plane=False
         )
 
 
     def construct(self):
-        #splane = NumberPlane()
-        #v_1 = Vector([1,1])
-        #v_2 = Vector([-2,1])
-        #self.add(plane, v_1, v_2)
 
-        matrix = [[1/2, -1], [1, 1]]
+        dot_1 = Dot(point=RIGHT + UP)
+        dot_2 = Dot(point=2*RIGHT + 2*UP)
+        self.add_moving_mobject(dot_1)
+        self.add_moving_mobject(dot_2)
+
+        matrix = rotation(-3*np.pi/2)
 
         self.apply_matrix(matrix)
         self.wait()
+
+#print(type(LEFT))
